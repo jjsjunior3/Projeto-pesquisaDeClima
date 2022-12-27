11c00eb796047db7f4ab2027dd0cf9ebcbe8bd1a
Junior Santos <junior.ead@hotmail.com>
2022-12-26 22:14:20 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,47 @@
+document.querySelector('.busca').addEventListener('submit', async (event)=>{
+    event.preventDefault();
+    let input = document.querySelector('#searchInput').value;
+
+    if(input !== '') {
+        clearInfor();
+        showWarning('Carregando...');
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=d06cdb298fafc83c520d5ab677fc477e&units=metric&lang=pt_br`;
+        let results = await fetch(url);
+        let json = await results.json();
+
+        if(json.cod === 200) {
+            showInfo({
+                name: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+                tempIcon: json.weather[0].icon,
+                windSpeed: json.wind.speed,
+                windAngle: json.wind.deg
+            })
+        } else {
+            clearInfor();
+            showWarning('Não encontramos está localização.');
+        }
+    } else {
+        clearInfor();
+    }
+});
+
+function showInfo(json) {
+    showWarning('');
+    document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
+    document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
+    document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed}<span>km/h</span>`;
+    document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
+    document.querySelector('.resultado').style.display = 'block';
+}
+
+function clearInfor() {
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none';
+}
+
+function showWarning(msg) {
+    document.querySelector('.aviso').innerHTML = msg;
+}
